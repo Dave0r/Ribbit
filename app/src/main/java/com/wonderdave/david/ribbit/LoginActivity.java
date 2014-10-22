97e0b16eb0711ded2d65b74096f80593521be3c0
Dave0r <david.barnes@aria-networks.com>
2014-10-22 13:15:25 +0100

    sdfs

--- app/src/main/java/com/wonderdave/david/ribbit/LoginActivity.java
+++ app/src/main/java/com/wonderdave/david/ribbit/LoginActivity.java
@@ -1,17 +1,27 @@
 package com.wonderdave.david.ribbit;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 
 public class LoginActivity extends Activity {
 
     protected TextView mSignUpTextView;
+    protected EditText mUsername;
+    protected EditText mPassword;
+    protected Button mloginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +34,33 @@ public class LoginActivity extends Activity {
             public void onClick(View view) {
                 Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
                 startActivity(intent);
+            }
+        });
+        mUsername = (EditText)findViewById(R.id.userNameField2);
+        mPassword = (EditText)findViewById(R.id.passwordField2);
+        mloginButton = (Button)findViewById(R.id.loginButton);
+        mloginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = mUsername.getText().toString();
+                String password = mPassword.getText().toString();
+
+                username = username.trim();
+                password = password.trim();
+
+
+                if (username.isEmpty() || password.isEmpty()){
+                    AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+                    builder.setMessage(R.string.login_error_message)
+                            .setTitle(R.string.login_error_title)
+                            .setPositiveButton(android.R.string.ok, null);
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+                }
+                else {
+                  //login
+                }
+
             }
         });
     }
