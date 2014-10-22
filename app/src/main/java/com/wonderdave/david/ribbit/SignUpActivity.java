cb5b06cb622324be88684761c5c22b01b70d54d8
Dave0r <david.barnes@aria-networks.com>
2014-10-22 11:45:24 +0100

    sdfasd

--- app/src/main/java/com/wonderdave/david/ribbit/SignUpActivity.java
+++ app/src/main/java/com/wonderdave/david/ribbit/SignUpActivity.java
@@ -3,12 +3,19 @@ package com.wonderdave.david.ribbit;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+
+import static android.app.AlertDialog.*;
 
 
 public class SignUpActivity extends Activity {
@@ -39,7 +46,7 @@ public class SignUpActivity extends Activity {
                 email = email.trim();
 
                 if (username.isEmpty() || password.isEmpty() || email.isEmpty()){
-                    AlertDialog.Builder builder = new AlertDialog.Builder(SignUpActivity.this);
+                    Builder builder = new Builder(SignUpActivity.this);
                     builder.setMessage(R.string.signup_error_message)
                         .setTitle(R.string.signup_error_title)
                         .setPositiveButton(android.R.string.ok, null);
@@ -47,7 +54,29 @@ public class SignUpActivity extends Activity {
                     dialog.show();
                 }
                 else {
-                    //create the new user
+                    ParseUser newUser = new ParseUser();
+                    newUser.setUsername(username);
+                    newUser.setPassword(password);
+                    newUser.setEmail(email);
+                    newUser.signUpInBackground(new SignUpCallback() {
+                        @Override
+                        public void done(ParseException e) {
+                            if (e == null){
+                                //success!
+                                Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                startActivity(intent);
+                            }else{
+                                Builder builder = new Builder(SignUpActivity.this);
+                                builder.setMessage(e.getMessage())
+                                        .setTitle(R.string.signup_error_title)
+                                        .setPositiveButton(android.R.string.ok, null);
+                                AlertDialog dialog = builder.create();
+                                dialog.show();
+                            }
+                        }
+                    });
                 }
 
             }
