3ca6d5fbabc5f6e391ffc2881a1660be1905fca6
Dave0r <david.barnes@aria-networks.com>
2014-10-22 14:57:49 +0100

    Added Progress Bar - Last check before I messed about

--- app/src/main/java/com/wonderdave/david/ribbit/LoginActivity.java
+++ app/src/main/java/com/wonderdave/david/ribbit/LoginActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -27,6 +28,7 @@ public class LoginActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
         setContentView(R.layout.activity_login);
 
         mSignUpTextView = (TextView)findViewById(R.id.signUpText);
@@ -59,9 +61,11 @@ public class LoginActivity extends Activity {
                     dialog.show();
                 }
                 else {
+                    setProgressBarIndeterminateVisibility(true);
                   ParseUser.logInInBackground(username, password, new LogInCallback() {
                       @Override
                       public void done(ParseUser user, ParseException e) {
+                          setProgressBarIndeterminateVisibility(false);
                           if (e == null) {
                             //login
                               Intent intent = new Intent(LoginActivity.this, MainActivity.class);
